eff33eb91f2c167b23c4a31b4ea808bfd69ad33d
or77iu <2945515386@qq.com>
2024-07-12 17:06:04 +0800

    feat:redux完整版

---
+++ 02_src_redux精简版版/components/Count/index.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+// 引入store
+import store from "../../redux/store";
+// 引入action
+import {
+  createDecrementAction,
+  createIncrementAction,
+} from "../../redux/count_action";
+export default class Count extends Component {
+  /*
+  componentDidMount() {
+    // redux中的数据变化，不会引起页面的更新
+    // 检测redux中状态的变化，只要变化，重新调用render
+    store.subscribe(() => {
+      this.setState({});
+    });
+  }
+      */
+  //   加法
+  increment = () => {
+    const { value } = this.selectNumber;
+    store.dispatch(createIncrementAction(value * 1));
+  };
+  //   减法
+  decrement = () => {
+    const { value } = this.selectNumber;
+    store.dispatch(createDecrementAction(value * 1));
+  };
+  //   奇数加
+  incrementIfOdd = () => {
+    const count = store.getState();
+    const { value } = this.selectNumber;
+    if (count % 2 !== 0) store.dispatch(createIncrementAction(value * 1));
+  };
+  //   异步加
+  incrementAsync = () => {
+    setTimeout(() => {
+      const { value } = this.selectNumber;
+      store.dispatch(createIncrementAction(value * 1));
+    }, 500);
+  };
+  render() {
+    return (
+      <div>
+        <h1>当前求和为{store.getState()}</h1>
+        <select ref={(c) => (this.selectNumber = c)} name="" id="">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        &nbsp; &nbsp;
+        <button onClick={this.increment}>+</button> &nbsp; &nbsp;
+        <button onClick={this.decrement}>-</button> &nbsp; &nbsp;
+        <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button> &nbsp;
+        &nbsp;
+        <button onClick={this.incrementAsync}>异步加</button> &nbsp; &nbsp;
+      </div>
+    );
+  }
+}
---
+++ 03_src_redux完整版/components/redux/store.js
@@ -0,0 +1,10 @@
+// 引入createStore,用于创建store对象
+import { legacy_createStore as createStore } from "redux";
+// 引入为Count组件服务的reducer
+import countReducer from "./count_reducer";
+export default createStore(countReducer);
+/*
+
+ redux 发布 4.2.0 版本，该版本将原始 createStore API 标记为 @deprecated(废弃)，并且添加了一个全新的 legacy_createStore API，
+ 但是并没有添加弃用警告。此外该版本鼓励用户迁移到 Redux Toolkit。
+*/
