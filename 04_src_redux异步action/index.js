72e7a2d1d85cdc54275101fd8e45786330a27069
or77iu <2945515386@qq.com>
2024-07-12 17:53:35 +0800

    feat:redux异步action

---
+++ 04_src_redux异步action/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "./app";
+import store from "./components/redux/store";
+// 由于React团队在2022年3月29日推出了react 18，所以ReactDOM.render()方法不再支持，控制台会报出警告
+// import ReactDOM from "react-dom";
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// 使用createRoot
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
+store.subscribe(() => {
+  root.render(<App />);
+});
